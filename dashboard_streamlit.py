1c64dbd5a746e4fa691480f74ce21dc9b31cee2b
jfoxdata <78475138+jfoxdata@users.noreply.github.com>
2021-04-22 13:15:53 +0200

    Update dashboard_streamlit.py

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import chart_studio.plotly as py
 import streamlit as st
 from sklearn.preprocessing import StandardScaler
 import plotly.express as px
